f8ae111334f13aec05d4dd15a98443074ea2d5d1
Phurithat Komkham <99815333+phu-phurithat@users.noreply.github.com>
2024-12-13 16:44:23 +0700

    Delete crudapi directory

--- crudapi/src/main/java/com/phu/crudapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.phu.crudapi.repository;
-
-import com.phu.crudapi.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
